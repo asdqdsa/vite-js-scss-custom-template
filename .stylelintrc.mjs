ae7b12c4e05c0580080cb1dd146b55539be8b289
asdqdsa <asdqdsa@protonmail.com>
2025-09-21 05:22:11 +0400

    fix: stylelint

--- .stylelintrc.mjs
+++ .stylelintrc.mjs
@@ -1,4 +1,8 @@
 export default {
   customSyntax: 'postcss-scss',
   extends: ['stylelint-config-recommended-scss'],
+  plugins: ['stylelint-scss'],
+  rules: {
+    'nesting-selector-no-missing-scoping-root': null,
+  },
 };
